3f333a7e3a7894e52520acf84366dfbbdb5389ab
kahvicentee <karinavicente.dev@gmail.com>
2026-04-21 13:02:50 -0300

    Adicionando arquivo readme e função para calcular a idade

---
+++ README.md
@@ -0,0 +1,13 @@
+# Age Calculator App
+**"This challenge is made to sharpen your JavaScript and form validation skills."**
+<br>
+
+🚀 Challenge by [Frontend Mentor](https://www.frontendmentor.io?ref=challenge) <br>
+👩🏻‍💻 Coded by [Karina Vicente](https://github.com/kahvicentee) <br>
+🌐 Here you can see this [Project](https://kahvicentee.github.io/ageCalculatorApp/) <br>
+📅 Completed on April 21th, 2026 <br>
+
+## Built with
+- HTML5
+- CSS3
+- JavaScript (Vanilla)
--- scripts/main.js
+++ scripts/main.js
@@ -18,43 +18,120 @@ const yearTitle = document.querySelector('.title-year');
 
 let errorEmpty = "This field is required";
 
-function validations() {
-    // Day 
-    if (day.value === "") {
-        dayError.textContent = errorEmpty;
-        day.style.border = "1px #ff5757 solid";
-        dayTitle.style.color = "#ff5757";
-    } else if (parseInt(day.value) > 31) {
-        dayError.textContent = "Must be a valid day";
-        day.style.border = "1px #ff5757 solid";
-        dayTitle.style.color = "#ff5757";
-    }
+function isValidDate(day, month, year) {
+    const date = new Date(year, month - 1, day);
 
-    // Month 
-    if (month.value === "") {
-        monthError.textContent = errorEmpty;
-        month.style.border = "1px #ff5757 solid";
-        monthTitle.style.color = "#ff5757";
-    } else if (parseInt(month.value) > 12) {
-        monthError.textContent = "Must be a valid month";
-        month.style.border = "1px #ff5757 solid";
-        monthTitle.style.color = "#ff5757";
-    }
-
-    // Year 
-    if (year.value === "") {
-        yearError.textContent = errorEmpty;
-        year.style.border = "1px #ff5757 solid";
-        yearTitle.style.color = "#ff5757";
-    }
+    return (
+        date.getFullYear() == year &&
+        date.getMonth() == month - 1 &&
+        date.getDate() == day
+    );
 }
+
+function validateField(input, errorEl, titleEl, type) {
+    if(input.value === "") {
+        errorEl.textContent = errorEmpty;
+        input.style.border = "1px #ff5757 solid";
+        titleEl.style.color = "#ff5757";
+        return false;
+    }
+
+    if(type === "day" && parseInt(input.value) > 31) {
+        errorEl.textContent = "Must a valid day";
+        input.style.border = "1px #ff5757 solid";
+        titleEl.style.color = "#ff5757";
+        return false;
+    }
+
+    if(type === "month" && parseInt(input.value) > 12) {
+        errorEl.textContent = "Must be a valid month";
+        input.style.border = "1px #ff5757 solid";
+        titleEl.style.color = "#ff5757";
+        return false;
+    }
+
+    if (day.value && month.value && year.value) {
+        const d = parseInt(day.value);
+        const m = parseInt(month.value);
+        const y = parseInt(year.value);
+
+        const today = new Date();
+        const inpDate = new Date(y, m - 1, d);
+
+        //Impossible Date
+        if(!isValidDate(d, m, y)) {
+            errorEl.textContent = "Must be a valid date";
+            input.style.border = "1px #ff5757 solid";
+            titleEl.style.color = "#ff5757";
+            return false;
+        }
+
+        if(inpDate > today) {
+            errorEl.textContent = "Must be a valid date";
+            input.style.border = "1px #ff5757 solid";
+            titleEl.style.color = "#ff5757";
+            return false;
+        }
+    }
+
+    errorEl.textContent = "";
+    input.style.border = "";
+    titleEl.style.color = "";
+
+    return true;
+}
+
+function validations() {
+    const isDayValid = validateField(day, dayError, dayTitle, "day");
+    const isMonthValid = validateField(month, monthError, monthTitle, "month");
+    const isYearValid = validateField(year, yearError, yearTitle, "year");
+
+    return isDayValid && isMonthValid && isYearValid;
+}
+
+day.addEventListener('input', () => {
+    validateField(day, dayError, dayTitle, "day");
+})
+
+month.addEventListener('input', () => {
+    validateField(month, monthError, monthTitle, "month");
+})
+
+year.addEventListener('input', () => {
+    validateField(year, yearError, yearTitle, "year");
+})
 
 // Calculate Age
 function calcAge() {
-    validations();
+    if(!validations()) return;
 
     const birthDate = new Date(year.value, month.value - 1, day.value);
     const today = new Date();
+
+    let days = today.getDate() - birthDate.getDate();
+    let months = today.getMonth() - birthDate.getMonth();
+    let years = today.getFullYear() - birthDate.getFullYear();
+
+    if (days < 0) {
+        months--;
+
+        const lastMonthh = new Date(
+            today.getFullYear(),
+            today.getMonth(),
+            0
+        ).getDate()
+
+        days += lastMonthh;
+    }
+
+    if (months < 0 ) {
+        years --;
+        months += 12;
+    }
+
+    resDay.textContent = days;
+    resMonth.textContent = months;
+    resYear.textContent = years;
 }
 
 button.addEventListener('click', () => {
